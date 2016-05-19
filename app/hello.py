606793282863a029d5dceceb1b1c2f45b84a0732
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-19 14:49:35 +0300

    installed the flask script and added csrf enctyption keys

--- app/hello.py
+++ app/hello.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
+from datetime import datetime
 from flask.ext.bootstrap import Bootstrap
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from flask.ext.script import Manager
+from flask.ext.moment import Moment
 
-@app.route('/index')
+app = Flask(__name__)
+
+moment = Moment(app)
+bootstrap = Bootstrap(app)
+manager = Manager(app)
+
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
+    
 
